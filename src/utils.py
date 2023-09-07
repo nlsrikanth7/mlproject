bce9d423debe4b3740ca9b9783a635e39a9cbd6f
nlsrikanth7 <nlsrikanth@gmail.com>
2023-09-07 11:08:13 -0400

    Hypertuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 import pickle 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 from src.exception import CustomException
@@ -22,15 +23,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, X_test, y_train, y_test, models):
+def evaluate_models(X_train, X_test, y_train, y_test, models, param):
     try:
         # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size =0.2, random_state = 42)
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) # Train Model 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train, y_train) # Train Model 
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
