9bccaf54327047f89607724f512edd17daf45467
nlsrikanth7 <nlsrikanth@gmail.com>
2023-09-07 21:58:27 -0400

    Flask Web App

--- src/utils.py
+++ src/utils.py
@@ -26,6 +26,8 @@ def save_object(file_path, obj):
 def evaluate_models(X_train, X_test, y_train, y_test, models, param):
     try:
         # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size =0.2, random_state = 42)
+        # report = {}
+
         report = {}
 
         for i in range(len(list(models))):
@@ -35,22 +37,30 @@ def evaluate_models(X_train, X_test, y_train, y_test, models, param):
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
 
-
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            # model.fit(X_train, y_train) # Train Model 
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]]=test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
